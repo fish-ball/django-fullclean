288b83f0715acfd29ddc110664044b4116a69430
Alfred Huang <57082212@qq.com>
2016-06-15 06:26:34 +0800

    release 0.0.2

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='django-fullclean',
-    packages=('django_fullclean',),
-    version='0.0.1',
+    packages=['django_fullclean'],
+    version='0.0.2',
     description='Force django model call full_clean before save.',
     author='Alfred Huang',
     author_email='57082212@qq.com',
     url='https://github.com/fish-ball/django-fullclean',
-    download_url='https://github.com/fish-ball/django-fullclean/tarball/0.0.1',
-    keywords=('django', 'model', 'validator', 'full_clean'),
-    classifiers=(),
+    download_url='https://github.com/fish-ball/django-fullclean/tarball/0.0.2',
+    keywords=['django', 'model', 'validator', 'full_clean'],
+    classifiers=[],
     license='MIT License',
-    install_requires=('django',),
+    install_requires=['django'],
     platforms='any',
 )
