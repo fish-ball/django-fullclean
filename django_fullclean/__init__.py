2638c401087fbe5aa12e56ce37f5c19ad280a167
Alfred Huang <57082212@qq.com>
2016-06-15 00:25:02 +0800

    release 0.0.1

---
+++ django_fullclean/__init__.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+
+@receiver(pre_save)
+def pre_save_full_clean_handler(sender, instance, *args, **kwargs):
+    """ Force all models to call full_clean before save """
+    instance.full_clean()
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='django-fullclean',
+    packages=('django_fullclean',),
+    version='0.0.1',
+    description='Force django model call full_clean before save.',
+    author='Alfred Huang',
+    author_email='57082212@qq.com',
+    url='https://github.com/fish-ball/django-fullclean',
+    download_url='https://github.com/fish-ball/django-fullclean/tarball/0.0.1',
+    keywords=('django', 'model', 'validator', 'full_clean'),
+    classifiers=(),
+    license='MIT License',
+    install_requires=('django',),
+    platforms='any',
+)
