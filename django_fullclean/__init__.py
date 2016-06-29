4ec8de9dc53684ffb0c41406f2079d3c5e20c833
Alfred Huang <57082212@qq.com>
2016-06-29 19:34:29 +0800

    排除了 Session 不能保存的问题

--- django_fullclean/__init__.py
+++ django_fullclean/__init__.py
@@ -1,9 +1,11 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.contrib.sessions.models import Session
 
 
 @receiver(pre_save)
 def pre_save_full_clean_handler(sender, instance, *args, **kwargs):
     """ Force all models to call full_clean before save """
-    instance.full_clean()
+    if sender != Session:
+        instance.full_clean()
 
