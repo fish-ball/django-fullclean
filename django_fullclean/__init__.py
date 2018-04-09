a789c23ff13c52dc061b08c9215e57c909260b24
Richard Connon <richard@connon.me.uk>
2018-04-09 21:06:56 +0100

    Add support for a whitelist in project settings to restrict fullclean

--- django_fullclean/__init__.py
+++ django_fullclean/__init__.py
@@ -1,11 +1,18 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
+import inspect
 
 
 @receiver(pre_save)
 def pre_save_full_clean_handler(sender, instance, *args, **kwargs):
     """ Force all models to call full_clean before save """
     from django.contrib.sessions.models import Session
-    if sender != Session:
-        instance.full_clean()
 
+    allowed_by_whitelist = settings.FULLCLEAN_WHITELIST \
+            and any([sender.__module__.startswith(s) for s in settings.FULLCLEAN_WHITELIST])
+
+    if sender != Session and allowed_by_whitelist:
+        instance.full_clean()
