693f973188d52b0abe91681f7693a54d604f1a93
Richard Connon <richard@connon.me.uk>
2018-04-11 00:26:30 +0100

    Fix default behaviour with no whitelist

--- django_fullclean/__init__.py
+++ django_fullclean/__init__.py
@@ -11,8 +11,9 @@ def pre_save_full_clean_handler(sender, instance, *args, **kwargs):
     """ Force all models to call full_clean before save """
     from django.contrib.sessions.models import Session
 
-    allowed_by_whitelist = settings.FULLCLEAN_WHITELIST \
-            and any([sender.__module__.startswith(s) for s in settings.FULLCLEAN_WHITELIST])
+    whitelist = getattr(settings, 'FULLCLEAN_WHITELIST', [''])
+
+    allowed_by_whitelist = any([sender.__module__.startswith(s) for s in whitelist])
 
     if sender != Session and allowed_by_whitelist:
         instance.full_clean()
